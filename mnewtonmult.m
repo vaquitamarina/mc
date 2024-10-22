75a3a3ca66f775739d6dc51f78d0db8996876587
vaquitamarina <87457016+vaquitamarina@users.noreply.github.com>
2024-10-21 23:10:06 -0500

    Update mnewtonmult.m

--- mnewtonmult.m
+++ mnewtonmult.m
@@ -1,22 +1,18 @@
 f_input = input('Ingrese las funciones, ej: [x(1)^2 + x(2)^2 - 1;x(1)^2 - x(2)]', 's');
-f = str2func(['@(x) ', f_input]);
 
-syms x;
+v = sym('x', [1 n]);
 f_sym = str2sym(f_input);
-j_sym = jacobian(f_sym, x);
-j = matlabFunction(j_sym);
-
-j = matlabFunction(f_sym, 'Vars', {x});
+j_sym = jacobian(f_sym, v);
 
 
-x0 = input('Introduce un vector inicial [x0, y0] (por ejemplo, [1, 0.5]): ');
+x0 = input('Introduce un vector inicial [x0 y0] (por ejemplo, [1 0.5]): ');
 
-e = input('Introduce la tolerancia (por ejemplo, 1e-6): ');
+e = input('Introduce el maximo error permisible: ');
 num = input('Introduce el número máximo de iteraciones: ');
 
 for i = 1:num
-  F_x = f(x0);
-  J_x = j(x0);
+  F_x = subs(f_sym,v,x0);
+  J_x = subs(j_sym,v,x0);
   dx = -J_x \ F_x;  
   x0 = x0 + dx';
   if norm(dx, inf) < e
