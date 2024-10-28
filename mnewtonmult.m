4d0238e74432bb724db7e630e959974e63028900
vaquitamarina <87457016+vaquitamarina@users.noreply.github.com>
2024-10-28 12:21:14 -0500

    Update mnewtonmult.m

--- mnewtonmult.m
+++ mnewtonmult.m
@@ -1,28 +1,29 @@
-f_input = input('Ingrese las funciones, ej: [x(1)^2 + x(2)^2 - 1;x(1)^2 - x(2)]', 's');
+clc
+clear 
+n = input('Ingrese el numero de variables: ');
+f_input = input('Ingrese las funciones, ej: [x1^2 + x2^2 - 1;x1^2 - x2]:  ', 's');
 
-v = sym('x', [1 n]);
+v = sym('x',[1 n]);
+
 f_sym = str2sym(f_input);
 j_sym = jacobian(f_sym, v);
 
 
-x0 = input('Introduce un vector inicial [x0 y0] (por ejemplo, [1 0.5]): ');
-
-e = input('Introduce el maximo error permisible: ');
-num = input('Introduce el número máximo de iteraciones: ');
-
+e = input('Introduce la tolerancia: ');
+num = input('Introduce el numero maximo de iteraciones: ');
+x0 = input('Ingrese el disparo inicial: ');
 for i = 1:num
   F_x = subs(f_sym,v,x0);
   J_x = subs(j_sym,v,x0);
   dx = -J_x \ F_x;  
   x0 = x0 + dx';
   if norm(dx, inf) < e
-    fprintf('Convergencia alcanzada en %d iteraciones.\n', i);
-    fprintf('Solucion aproximada: ');
-    x0
+    fprintf('Solucion aproximada:\n');
+    fprintf('%.10f\n', x0);
     break;
   end  
 end
 
 if(i == num)
-  disp('El método no convergió dentro del número máximo de iteraciones.');
+  disp('El metodo no convergio dentro del numero maximo de iteraciones.');
 end
