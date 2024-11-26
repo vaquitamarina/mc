45a0c9db1d28aed88605ed230a1ecd7cd58c0c49
vaquitamarina <87457016+vaquitamarina@users.noreply.github.com>
2024-11-26 08:19:29 -0500

    Update aproxmiacion.m

--- aproxmiacion.m
+++ aproxmiacion.m
@@ -1,48 +1,61 @@
-clc
-clear
-disp('Aproximacion polinomial');
-n = input('Ingrese el numero de datos a ingresar: ');
-m = input('Ingrese el grado del polinomio');
-s = 0;
-for i=1:n
+clc;
+clear;
+
+n = input('Ingrese el número de datos a ingresar: ');
+m = input('Ingrese el grado del polinomio: ');
+ys = 0;
+
+for i = 1:n
     fprintf('Ingrese el valor de x(%d): ', i);
     x(i) = input('');
     fprintf('Ingrese el valor de y(%d): ', i);
     y(i) = input('');
-    s = s + y(i);
+    ys = ys + y(i);
 end
 
-for k=0:(2*m)
-    s(k+1) = 0;
-    for i=1:n
-        s(k+1) = s(k+1) + (x(i)^k+1);
+for k = 0:2*m
+    s(k+1) = 0; 
+    for i = 1:n
+        s(k+1) = s(k+1) + x(i)^k; 
     end
 end
 
-for k=0:m
+for k = 0:m
     c(k+1) = 0;
-    for i=1:n
-        c(k+1) = c(k+1) + (x(i)^k)*y(i);
+    for i = 1:n
+        c(k+1) = c(k+1) + (x(i)^k) * y(i);
     end
 end
 
-for i=1:m+1
-    for j=i:m+1
-        d(i, j) = s(i+j-1);
-        d(j, i) = d(i,j);
+for i = 1:m+1
+    for j = i:m+1
+        d(i, j) = s(i+j-1); 
     end
+    d(j,i) = d(i,j);
 end
 
-a = c/d;
+a = d \ c'; 
 
-fprintf('y = %d + ',a(1));
-for i=2:m+1
-   fprintf('%d x^(%d)+ ', a(i),i-1);
+fprintf('y = %.4f', a(1));
+for i = 2:m+1
+    fprintf(' + %.4f*x^%d', a(i), i-1);
 end
+fprintf('\n');
 
 sm = 0;
 sr = 0;
-yr = 0;
-for i=1:n
-    
+yr = ys/n; 
+for i = 1:n
+    sm = sm + (y(i) - yr)^2;
+    yp = 0;
+    for k = 0:m
+        yp = yp + a(k+1) * x(i)^k;
+    end
+    sr = sr + (y(i) - yp)^2;
 end
+
+rr = (sm-sr)/sm; 
+r = sqrt(rr);       
+
+fprintf('Coeficiente de determinación (R^2): %.4f\n', rr);
+fprintf('Coeficiente de correlación (R): %.4f\n', r);
