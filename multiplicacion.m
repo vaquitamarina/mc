87ca8b2a84ba81507d42f3833a6c3a26faeb5acd
vaquitamarina <87457016+vaquitamarina@users.noreply.github.com>
2024-11-26 01:02:01 -0500

    Update multiplicacion.m

--- multiplicacion.m
+++ multiplicacion.m
@@ -1,7 +1,10 @@
-m = input('Ingrese el numero de columnas de la primera matriz: ');
-n = input('Ingrese el numero de filas de la primera matriz: ');
-p = input('Ingrese el numero de columnas de la segunda matriz: ');
-q = input('Ingrese el numero de filas de la segunda matriz: ');
+clc
+clear 
+
+n = input('Ingrese el numero de columnas de la primera matriz: ');
+m = input('Ingrese el numero de filas de la primera matriz: ');
+q = input('Ingrese el numero de columnas de la segunda matriz: ');
+p = input('Ingrese el numero de filas de la segunda matriz: ');
 
 if(n==p)
     for i=1:m
@@ -12,7 +15,7 @@ if(n==p)
     end
     for i=1:p
         for j=1:q
-            fprintf('Ingrese el valor de B(%d)(%d): ', p,q);
+            fprintf('Ingrese el valor de B(%d)(%d): ', i,j);
             b(i,j) = input('');
         end
     end
