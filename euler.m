6aee5b28b4d24be765bd82d8c291bd5ec43056a9
vaquitamarina <87457016+vaquitamarina@users.noreply.github.com>
2024-11-26 06:58:59 -0500

    Update euler.m

--- euler.m
+++ euler.m
@@ -1,22 +1,19 @@
-clc;
-clear;
+clc
+clear
 
 f_input = input('Ingrese la función f(x, y), por ejemplo: x^2 + y^2: ', 's');
 f = str2func(['@(x, y) ' f_input]);  
 
 n = input('Ingrese el número de particiones (n): ');
-x0 = input('Ingrese el valor inicial x0: ');
-y0 = input('Ingrese el valor inicial y0: ');
+x(1) = input('Ingrese el valor inicial x0: ');
+y(1) = input('Ingrese el valor inicial y0: ');
 xn = input('Ingrese el valor final xn: ');
 
-h = (xn - x0) / n;
-
-x(1) = x0;
-y(1) = y0;
+h = (xn - x(1)) / n;
 
 for i = 2:n+1
     y(i) = y(i-1) + h * f(x(i-1), y(i-1));  
-    x(i) = x0 + (i-1) * h;                
+    x(i) = x(1) + (i-1) * h;                
 end
 
 fprintf('El valor aproximado de y(%d) es: %d ', xn, y(xn+1));
